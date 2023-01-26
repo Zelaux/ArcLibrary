842918699f204c91b1a8d3112fca5a8d4f509a64
Zelaux <ura20060605@gmail.com>
2023-01-26 22:48:44 +0300

    Added settings key listener

--- settings/src/zelaux/arclib/settings/SettingKey.kt
+++ settings/src/zelaux/arclib/settings/SettingKey.kt
@@ -1,7 +1,8 @@
 package zelaux.arclib.settings
 
-import arc.*
-import arc.struct.*
+import arc.Core
+import arc.func.Cons2
+import arc.struct.Seq
 
 open class SettingKey<T>(
     @JvmField val key: String,
@@ -18,10 +19,16 @@ open class SettingKey<T>(
         allKeys.add(this)
     }
 
+    val updateListeners: Seq<Cons2<T, SettingKey<T>>> = Seq();
 
     open fun def() = defaultProvider()
     fun get(): T = valueGetter(this)
-    fun set(value: T) = valueSetter(value, this)
+    fun set(value: T) {
+        valueSetter(value, this)
+        for (listener in updateListeners) {
+            listener[value, this]
+        }
+    }
 
     open fun setDefault() {
         Core.settings.defaults(key, def())
