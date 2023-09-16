5d8fe4e3027bb00c8fa13424701d4ec79b89a355
Zelaux <ura20060605@gmail.com>
2023-09-16 19:39:14 +0300

    Added set methods for Entries

--- utils/entries/src/arclibrary/utils/entries/Entries.java
+++ utils/entries/src/arclibrary/utils/entries/Entries.java
@@ -83,5 +83,49 @@ public class Entries {
     public static <K, V> ObjectMap.Entry<K, V> copy(ObjectMap.Entry<K, V> entry) {
         return objectEntry(entry.key, entry.value);
     }
+
+    //endregion
+    //region set
+    public static IntFloatMap.Entry set(IntFloatMap.Entry entry, int key, float value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static IntIntMap.Entry set(IntIntMap.Entry entry, int key, int value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static <V> IntMap.Entry<V> set(IntMap.Entry<V> entry, int key, V value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static <V> LongMap.Entry<V> set(LongMap.Entry<V> entry, long key, V value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static <K> ObjectFloatMap.Entry<K> set(ObjectFloatMap.Entry<K> entry, K key, float value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static <K> ObjectIntMap.Entry<K> set(ObjectIntMap.Entry<K> entry, K key, int value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
+
+    public static <K, V> ObjectMap.Entry<K, V> set(ObjectMap.Entry<K, V> entry, K key, V value) {
+        entry.key = key;
+        entry.value = value;
+        return entry;
+    }
 //endregion
 }
